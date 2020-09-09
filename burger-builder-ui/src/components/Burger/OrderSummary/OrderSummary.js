3a4005bac94375ce6b780c4b530509792f1d2422
gexiaoquan <xiaoquange91@gmail.com>
2020-09-09 15:14:52 -0500

    Added buttons and price to order summary

--- burger-builder-ui/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder-ui/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../UI/Button/Button';
 
 const orderSummary = (props) => {
     const ingredientItems = Object.keys(props.ingredients).map(
@@ -13,6 +14,10 @@ const orderSummary = (props) => {
             <h3>Your Order Summary</h3>
             <p>Ingredients:</p>
             <ul>{ingredientItems}</ul>
+            <p><strong>Total Price: {props.price}</strong></p>
+            <p>Continue to Checkout?</p>
+            <Button buttonType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
+            <Button buttonType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
         </React.Fragment>
     )
 }
